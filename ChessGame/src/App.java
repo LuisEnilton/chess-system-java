b90d51c2860f30e1e37b0a111e0c96183072eac1
Luis Enilton <110181744+LuisEnilton@users.noreply.github.com>
2023-05-23 20:41:14 -0300

    First Class Position

--- ChessGame/src/App.java
+++ ChessGame/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         Position position = new Position(0, 0);
+         System.out.println(position);
+
     }
 }
