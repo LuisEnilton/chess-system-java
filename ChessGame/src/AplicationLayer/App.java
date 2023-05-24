8563e3ff9534c74e2818ceff237af71af12cb64d
Luis Enilton <110181744+LuisEnilton@users.noreply.github.com>
2023-05-24 20:22:00 -0300

    Create the UI

---
+++ ChessGame/src/AplicationLayer/App.java
@@ -0,0 +1,11 @@
+package AplicationLayer;
+import BoardLayer.Board;
+import ChessLayer.ChessMatch;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Board board = new Board(8,8);
+        ChessMatch match = new ChessMatch(board);
+        UI.printBoard(match.getPieces());
+    }
+}
--- ChessGame/src/App.java
+++
@@ -1,8 +0,0 @@
-import BoardLayer.Board;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-         Board board = new Board(8,8);
-         System.out.println(board.getColumns());
-    }
-}
