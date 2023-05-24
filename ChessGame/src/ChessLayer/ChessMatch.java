1249d1ea76eef02753e2144a04059610bedefa00
Luis Enilton <110181744+LuisEnilton@users.noreply.github.com>
2023-05-24 20:48:03 -0300

    add Pieces and placePieces

--- ChessGame/src/ChessLayer/ChessMatch.java
+++ ChessGame/src/ChessLayer/ChessMatch.java
@@ -1,6 +1,9 @@
 package ChessLayer;
 
 import BoardLayer.Board;
+import BoardLayer.Position;
+import ChessLayer.Pieces.King;
+import ChessLayer.Pieces.Rook;
 
 public class ChessMatch {
     private int turn;
@@ -9,6 +12,7 @@ public class ChessMatch {
 
     public ChessMatch(Board board) {
         this.board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces(){
@@ -19,6 +23,12 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup(){
+        board.placePiece(new Rook(board,Color.White), new Position(2,1 ));
+        board.placePiece(new King(board, Color.Black), new Position(0,4));
+        board.placePiece(new King(board, Color.Black), new Position(7,4));
     }
 
     
